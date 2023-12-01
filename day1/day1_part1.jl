d849e85b2a9a954b103c73baa7e31f3492f69be1
C-Kitching <113296322+C-Kitching@users.noreply.github.com>
2023-12-01 16:43:38 +0000

    julia

--- day1/day1_part1.jl
+++ day1/day1_part1.jl
@@ -1,24 +1,27 @@
-# read file
-file_path = "day1/day1.txt"
-file = open(file_path, "r")
+function day1_part1()
+    # read file
+    file_path = "day1/day1.txt"
+    file = open(file_path, "r")
 
-# store numeric parts as integers
-numeric_parts = Int[]
+    # store numeric parts as integers
+    numeric_parts = Int[]
 
-# regex to match numeric parts
-regex = r"\d"
+    # regex to match numeric parts
+    regex = r"\d"
 
-# parse each line
-for line in eachline(file)
+    # parse each line
+    for line in eachline(file)
 
-    # extract numeric part
-    first_digit = parse(Int, match(r"\d", line).match)
-    last_digit = parse(Int, match(r"\d", reverse(line)).match)
-    push!(numeric_parts, first_digit*10+last_digit)
+        # extract numeric part
+        first_digit = parse(Int, match(r"\d", line).match)
+        last_digit = parse(Int, match(r"\d", reverse(line)).match)
+        push!(numeric_parts, first_digit*10+last_digit)
+    end
+
+    # close file
+    close(file)
+
+    # print result
+    println(sum(numeric_parts))
+
 end
-
-# close file
-close(file)
-
-# print result
-println(sum(numeric_parts))
