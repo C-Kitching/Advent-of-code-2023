24a7d452d348be6ad980f96f49fcd3c4bef709d6
C-Kitching <113296322+C-Kitching@users.noreply.github.com>
2023-12-17 13:35:41 +0000

    day17

--- day17/day17_part1.jl
+++ day17/day17_part1.jl
@@ -1,7 +1,28 @@
 
 
+# define state of each cell
+struct Cell
+    cost::Int # cost to reach cell 
+    prev::Tuple{Int, Int} # coords of previous cell
+    steps::Int # number of steps taken in current direction
+    dir::Symbol # direction of last move
+end
+
 # main function
 function day17_part1()
+
+    # read data
+    lines = readlines("input/day17_test.txt")
+    grid = Vector{String}()
+    for line in lines
+        push!(grid, line)
+    end
+    rows = length(grid)
+    cols = length(grid[0])
+
+
+
+
 
 
 end
